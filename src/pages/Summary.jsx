cd71141768acea8e844e55c22827673993cc776a
mayog <dhtnthu9794048@gmail.com>
2025-05-30 01:06:56 +0300

    Change style of loading doc

--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -98,7 +98,7 @@ const LoadingPlaceholder = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: white;
+  background: rgba(10, 10, 10, 0.5);
   color: #d4af37;
   font-size: 1.2rem;
   flex-direction: column;
